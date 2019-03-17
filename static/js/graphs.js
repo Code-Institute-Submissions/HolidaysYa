dbdc631ef99a409590c13a1e744ae832006a0d0d
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-03-17 19:22:36 +0000

    filters by month and budget added

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,41 +1,65 @@
 queue()
     .defer(d3.csv, "data/Data.csv")
-    .await(createGraphics);
+    .await(getMonth);
 
-//var optionText =  $('#monthSelector option:selected').text();
 
-function createGraphics(error, data){
-   
-   $( "#monthSelector" ).change(function() {
-   createGraphics2(data);
-}); 
-    
+function getMonth(error, data) {
+    //console.log(data);
+
+    // only call "createGraphics" the month value after the dropdown option has been selected
+    $("#monthSelector").change(function () {
+        filterData(data);
+    });
 }
 
 
+function filterData(data) {
 
-function createGraphics2(error, data) {
-
-    //first we need to extract the data corresponding to the month selected
     //get the text for the option selected
-
-  
     var monthSelected = $('#monthSelector :selected').text();
-      console.log(monthSelected);
- 
-    var dataMonth = data.filter(function(element) {
-        return (element.month === 'March');
+
+
+    //this will create an object only containing the data for the month
+    var dataMonth = data.filter(function (element) {
+        return (element.month === monthSelected);
     });
 
-    //console.log(monthSelected);
-
-    var ndx = crossfilter(dataMonth);
-
-    createBudgetCharts(ndx);
-
-    dc.renderAll();
+    //https://www.jstips.co/en/javascript/passing-arguments-to-callback-functions/
+    document.getElementById("resultsBudget").addEventListener("click", filterByBudget(dataMonth));
 }
 
+function filterByBudget(dataMonth) {
+    return function () {
+        var maxBudgetValue = parseInt(document.getElementById("maxBudget").value);
+        //var maxBudgetNumber = parseInt(maxBudgetValue);
+
+        var dataBudget = dataMonth.filter(function (element) {
+            return (parseInt(element.maxBudget) < maxBudgetValue);
+        });
+
+        //for testing
+        console.log(typeof dataBudget);
+        console.log(typeof maxBudgetValue);
+
+           
+
+        if (maxBudgetValue<50) {  // update the 50 with the minimun value from the dataset once I know how to
+            alert('Europe is not that cheap, sorry!'); //also remove the charts in the screen
+        } else{
+            createDataForGraphics(dataBudget);
+        }
+
+    }
+};
+
+
+
+function createDataForGraphics(data) {
+    console.log(data);
+    var ndx = crossfilter(data);
+    createBudgetCharts(ndx);
+    dc.renderAll();
+}
 
 
 
