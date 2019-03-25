3c5992a2aeaa1683f82c2ee768a8a8f5af3c1c16
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-03-25 18:54:02 +0000

    code cleaned, remove charts, inputs and messages for next seacrh

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -5,48 +5,40 @@ queue()
 
 function getMonth(error, data) {
 
-    // only call "createGraphics" the month value after the dropdown option has been selected
+    if (error) {  /////////// improve this???
+        document.getElementById("error").innerHTML = `<h2 class="text-danger">Error retrieving the data file!</h2>`;
+    }
+
+    //Don't show the input fields unless the buttons are pressed
+    $(".budgetSelect").hide();
+    $(".weatherSelect").hide();
+
+
+    // only call "filterData" after the dropdown option has been selected
     $("#monthSelector").change(function () {
         filterData(data);
     });
 }
 
-function showTitleBudget() {
-    document.getElementById("BudgetTitle").innerHTML=`<h2>Results By Budget</h2>`;
-    document.getElementById("resultsBudget").innerHTML=`New Search`;
-}
-
- function showTitleWeather() {
-     document.getElementById("WeatherTitle").innerHTML=`<h2>Results By Weather</h2>`;
-     document.getElementById("resultsTemp").innerHTML=`New Search`;
- }
-
-
 
 
 function filterData(data) {
 
+    document.getElementById("chooseBudget").addEventListener("click", optionBudget());
+    document.getElementById("chooseWeather").addEventListener("click", optionWeather());
+
     //get the text for the option selected
     var monthSelected = $('#monthSelector :selected').text();
 
-    ///////CHECK WHAT HAPPENS WHEN IS JANUARY!!!
+    //////////////////////////////////////////////////////////CHECK WHAT HAPPENS WHEN IS JANUARY!!!
     //this will create an object only containing the data for the month
     var dataMonth = data.filter(function (element) {
         return (element.month === monthSelected);
     });
 
     //convert to number the number fields
-    dataMonth.forEach(function (d) {
-        d.hostelNight = parseInt(d.hostelNight);
-        d.meals = parseInt(d.meals);
-        d.drinks = parseInt(d.drinks);
-        d.transport = parseInt(d.transport);
-        d.attractions = parseInt(d.attractions);
-        d.visitorsCity = parseInt(d.visitorsCity);
-        d.minTemp = parseInt(d.minTemp);
-        d.maxTemp = parseInt(d.maxTemp);
+    convertToInteger(dataMonth);
 
-    })
 
     //https://www.jstips.co/en/javascript/passing-arguments-to-callback-functions/
     document.getElementById("resultsBudget").addEventListener("click", filterByBudget(dataMonth));
@@ -59,12 +51,19 @@ function filterData(data) {
 
 function filterByBudget(dataMonth) {
     return function () {
+        //removes the inputs in case we want to seach again by weather
+        $('input[name=minTemp').val('');
+        $('input[name=maxTemp').val('');
 
         var filteredBy = 'Budget';
+        $(".weather").hide();
+        $(".common").show();
 
-        //get input value
+
+
+        //get input value and converte it to number
         var maxBudgetValue = parseInt(document.getElementById("maxBudget").value);
-        //add validation!! not empty and number
+        /////////////////add validation!! not empty and number
 
         //this will create an Object only containing the cities that fit the budget
         var totalBudget = 0;
@@ -77,63 +76,62 @@ function filterByBudget(dataMonth) {
             return (totalBudget < maxBudgetValue);
         });
 
-        //If there is one or more cities matching the criteria it will call the function createDataForGraphics(dataBudget);
-        if (checkIfObjectEmpty(dataBudget)) {
-            document.getElementById("infoMessage").innerHTML=`<h2 class="text-danger">We're sorry but Europe is not that cheap!</h2>`;
-            $("#budgetCharts").hide();
+        //If there is one or more cities matching the criteria it will call 
+        //the function "createDataForGraphics" if not it will display a message;
 
-        }
-        else {
-            document.getElementById("infoMessage").innerHTML="";
-            $("#budgetCharts").show();
-            createDataForGraphics(dataBudget, filteredBy);
-        }
-
-       
-
+        citiesMatchingCriteria(dataBudget, filteredBy);
     }
 };
 
 
 function filterByWeather(dataMonth) {
     return function () {
+        //removes the input in case we want to seach again by weather
+        $('input[name=maxBudget').val('');
 
         var filteredBy = 'Weather';
+        $(".budget").hide();
+        $(".common").show();
 
         //get input values
         var minTemp = document.getElementById("minTemp").value;
         var maxTemp = document.getElementById("maxTemp").value;
-
-        //add validation!! not empty and number
-
-
-        //this will create an Object only containing the cities that fit the budget
+       
         
+            //this will create an Object only containing the cities that have that temperature
             var dataWeather = dataMonth.filter(function (element) {
-            return (minTemp >= element.minTemp && maxTemp <= element.maxTemp);
-        });
+                return (minTemp >= element.minTemp && maxTemp <= element.maxTemp);
+            });
+            //If there is one or more cities matching the criteria it will call 
+            //the function "createDataForGraphics" if not it will display a message;
 
-
-        //If there is one or more cities matching the criteria it will call the function createDataForGraphics(dataBudget);
-        if (checkIfObjectEmpty(dataWeather)) {
-            document.getElementById("infoMessageWeather").innerHTML=`<h2 class="text-danger">We're sorry but we don't cities with that average weather</h2>`;
-            $("#weatherCharts").hide();
-
-        }
-        else {
-            document.getElementById("infoMessageWeather").innerHTML="";
-            $("#weatherCharts").show();
-            createDataForGraphics(dataWeather, filteredBy);
-        }
-
-        //checks if the value object after filtering by budget is empty
-        
-
+           citiesMatchingCriteria(dataWeather, filteredBy);
+      
     }
 };
 
+
+function citiesMatchingCriteria(data, filteredBy) {
+    document.getElementById("infoMessage").innerHTML = "";
+
+    if (checkIfObjectEmpty(data)) {
+
+        if (filteredBy == "Budget") {
+            document.getElementById("infoMessage").innerHTML = `<h2 class="text-danger">We're sorry but Europe is not that cheap!</h2>`;
+        } else {
+            document.getElementById("infoMessage").innerHTML = `<h2 class="text-danger">We're sorry but we don't cities with that average weather</h2>`;
+        }
+        $("#charts").hide();
+    }
+    else {
+        $("#charts").show();
+        createDataForGraphics(data, filteredBy);
+    }
+}
+
+
 //this function will check if there is or there isn't matching results in the data
-//if the object is empty meand that not matches where found
+//if the object is empty means that not matches where found
 function checkIfObjectEmpty(data) {
     for (var key in data) {
         if (data.hasOwnProperty(key))
@@ -144,13 +142,10 @@ function checkIfObjectEmpty(data) {
 
 
 
-
-
-
 function createDataForGraphics(data, filteredBy) {
     var ndx = crossfilter(data);
 
-
+    //Budget graphics
     if (filteredBy === 'Budget') {
         fiterByCountry(ndx);
         createCurrencyChart(ndx);
@@ -158,10 +153,17 @@ function createDataForGraphics(data, filteredBy) {
         createTotalDailyBudget(ndx);
         createCorrelationCharts(data, ndx)
     }
+
+    //weather graphics
     if (filteredBy === 'Weather') {
+        fiterByCountry(ndx);
+        createCurrencyChart(ndx);
+        createCityChart(ndx);
         createTempCityChart(ndx);
+
+        // createTempCityChart(ndx);
     }
-   
+
 
     dc.renderAll();
 }
@@ -169,17 +171,15 @@ function createDataForGraphics(data, filteredBy) {
 
 
 function fiterByCountry(ndx) {
-    {
-        var dim = ndx.dimension(dc.pluck('country'));
-        var group = dim.group();
 
-        dc.selectMenu('#countrySelector')
-            .multiple(true)
-            .dimension(dim)
-            .group(group);
+    var dim = ndx.dimension(dc.pluck('country'));
+    var group = dim.group();
 
-    }
-}
+    dc.selectMenu('#countrySelector')
+        .multiple(true)
+        .dimension(dim)
+        .group(group);
+};
 
 
 function createCurrencyChart(ndx) {
@@ -193,7 +193,7 @@ function createCurrencyChart(ndx) {
         .dimension(dimCurrency)
         .group(groupCurrency)
         .transitionDuration(1500);
-}
+};
 
 function createCityChart(ndx) {
 
@@ -206,7 +206,7 @@ function createCityChart(ndx) {
         .dimension(dimCity)
         .group(groupCity)
         .transitionDuration(1500);
-}
+};
 
 function createTotalDailyBudget(ndx) {
 
@@ -239,63 +239,109 @@ function createTotalDailyBudget(ndx) {
         .xAxisLabel('City')
         .yAxisLabel('Total daily budget')
         .yAxis().ticks(8);
-    //.legend(dc.legend().x(320).y(20).itemHeight(15).gap(5));
-    // .margins({top: 10, right: 50, bottom: 30, left: 40});
+    //??????.legend(dc.legend().x(320).y(20).itemHeight(15).gap(5));
+    //?????? .margins({top: 10, right: 50, bottom: 30, left: 40});
 
-}
+};
 
 
 
 function createCorrelationCharts(data, ndx) {
-
-    //var dimArrivals = ndx.dimension(dc.pluck('visitorsCity'));
-
-    data.forEach(function (d) {
-        d.column1 = +d.column1;
-    });
 
     var maxArrivals = d3.max(data, function (d) { return d.visitorsCity; });
 
     var dimBudget = ndx.dimension(function (d) {
         var totalBudget = d.hostelNight + d.meals + d.drinks + d.attractions + d.transport;
         return [d.visitorsCity, totalBudget, d.city];
-        // return [d.hostelNight];
-
     })
 
     var budgetGroup = dimBudget.group();
 
-  /*  var cityColours = d3.scale.ordinal()
-        .domain(["Vienna", "Brussels", "Prague", "Copenhagen", "Helsinki", "Paris", "Berlin", "Athens",
-            "Budapest", "Dublin", "Milan", "Rome", "Amsterdam", "Oslo", "Warsaw", "Lisbon",
-            "Barcelona", "Madrid", "Stockholm", "Istanbul", "Edinburgh", "London"])
-        .range(["#FBEC5D", "#F2C649", "#6050DC", "#0BDA51", "#979AAA", "#F37A48", "#FDBE02", "#FF8243",
-            "#74C365", "#880085", "#EAA221", "#C32148", "#800000", "#B03060", "#E0B0FF", "#915F6D", "#EF98AA",
-            "#47ABCC", "#30BFBF", "#ACACE6", "#5E8C31", "#D9E650"]);
-*/
+    // var cityColours = d3.scale.ordinal()
+    //     .domain(["Vienna", "Brussels", "Prague", "Copenhagen", "Helsinki", "Paris", "Berlin", "Athens",
+    //         "Budapest", "Dublin", "Milan", "Rome", "Amsterdam", "Oslo", "Warsaw", "Lisbon",
+    //         "Barcelona", "Madrid", "Stockholm", "Istanbul", "Edinburgh", "London"])
+    //     .range(["#FBEC5D", "#F2C649", "#6050DC", "#0BDA51", "#979AAA", "#F37A48", "#FDBE02", "#FF8243",
+    //         "#74C365", "#880085", "#EAA221", "#C32148", "#800000", "#B03060", "#E0B0FF", "#915F6D", "#EF98AA",
+    //         "#47ABCC", "#30BFBF", "#ACACE6", "#5E8C31", "#D9E650"]);
+
 
     dc.scatterPlot("#correlation")
-        .height(120) //breading space around pie chart, dont confuse with diameter
+        .height(120)
         .width(400)
         .x(d3.scale.linear().domain([0, maxArrivals]))
         .brushOn(false)
         .symbolSize(8)
         .clipPadding(10)
-        .xAxisLabel('Number of country visitor (2017 or 2018)')
+        .xAxisLabel('Number of country visitors (2017 or 2018)')
         .yAxisLabel("Budget")
         .title(function (d) {
             return "In " + d.key[2] + " you will need a daily bugget of " + d.key[1] + " \nand there was " + d.key[0] + " millions visits in 2017";
         })
-     /*   .colorAccessor(function (d) {
-            return d.key[0];
-        })
-        .colors(cityColours)*/
+        // .colorAccessor(function (d) {
+        //     return d.key[0];
+        // })
+        // .colors(cityColours)
         .dimension(dimBudget)
         .group(budgetGroup);
 }
 
-function createTempCityChart(ndx){
+function createTempCityChart(ndx) {
 
 }
 
 
+
+///////////// other functions
+
+
+function convertToInteger(dataMonth) {
+    dataMonth.forEach(function (d) {
+        d.hostelNight = parseInt(d.hostelNight);
+        d.meals = parseInt(d.meals);
+        d.drinks = parseInt(d.drinks);
+        d.transport = parseInt(d.transport);
+        d.attractions = parseInt(d.attractions);
+        d.visitorsCity = parseInt(d.visitorsCity);
+        d.minTemp = parseInt(d.minTemp);
+        d.maxTemp = parseInt(d.maxTemp);
+    })
+}
+
+function optionBudget() {
+    //remove inforMessage in case it has the previous message
+    document.getElementById("infoMessage").innerHTML = "";
+
+    //hide weather input boxes
+    $(".weatherSelect").hide();
+    //show budget input box
+    $(".budgetSelect").show();
+
+    //hide all charts and we will wait until the "search" button is clicked
+    $("#charts").hide();
+}
+
+function optionWeather() {
+    //remove inforMessage in case it has the previous message
+    document.getElementById("infoMessage").innerHTML = "";
+
+    //hide budget input box
+    $(".budgetSelect").hide();
+
+    //show weather input boxes
+    $(".weatherSelect").show();
+
+    //hide all charts and we will wait until the "search" button is clicked
+    $("#charts").hide();
+}
+
+//Charts Title depend on the button clicked option
+//will be "Budget or Weather"
+function showTitle(option) {
+    document.getElementById("Title").innerHTML = `<h2>Results By ${option}</h2>`;
+    if (option == "Budget") {
+        document.getElementById("resultsBudget").innerHTML = `New Search`;
+    } else {
+        document.getElementById("resultsTemp").innerHTML = `New Search`;
+    }
+}
