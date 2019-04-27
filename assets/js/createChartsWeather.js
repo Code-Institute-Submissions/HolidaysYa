c9872dc5b97aee5c33ce5ad8dc426af936c638b4
Elena Sacristán Sacristán <44655750+elenasacristan@users.noreply.github.com>
2019-04-27 15:00:10 +0100

    replace anonymous function

--- assets/js/createChartsWeather.js
+++ assets/js/createChartsWeather.js
@@ -10,7 +10,7 @@ function show_max_weather(ndx, column, element) {
 
     dc.numberDisplay(element)
         .group(dim_max_groupAll(maxDim, column))
-        .valueAccessor(x => x)
+        .valueAccessor(x)
         .formatNumber(d3.format('.0f'))
         .render();
 }
@@ -30,7 +30,7 @@ function show_min_weather(ndx, column, element) {
 
     dc.numberDisplay(element)
         .group(dim_min_groupAll(minDim, column))
-        .valueAccessor(x => x)
+        .valueAccessor(x)
         .formatNumber(d3.format('.0f'))
         .render();
 }
